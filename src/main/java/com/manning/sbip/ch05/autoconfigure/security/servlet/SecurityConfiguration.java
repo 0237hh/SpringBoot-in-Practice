ee8917a61fd681867047f1f9dda7387770db9a81
0237hh <153145663+0237hh@users.noreply.github.com>
2024-09-18 14:45:51 +0900

    용

--- src/main/java/com/manning/sbip/ch05/autoconfigure/security/servlet/SecurityConfiguration.java
+++ src/main/java/com/manning/sbip/ch05/autoconfigure/security/servlet/SecurityConfiguration.java
@@ -1,14 +1,11 @@
 package com.manning.sbip.ch05.autoconfigure.security.servlet;
 
-import com.manning.sbip.ch05.service.CustomUserDetailsService;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -20,18 +17,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated().and()
                 .formLogin().loginPage("/login");
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.ldapAuthentication()
+                .userDnPatterns("uid={0}, ou=people")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=manning,dc=com")
+                .and()
+                .passwordCompare()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .passwordAttribute("userPassword");
+    }
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/webjars/**", "/images/**", "/css/**", "/h2-console/**");
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new CustomUserDetailsService(){};
-    }
 }
